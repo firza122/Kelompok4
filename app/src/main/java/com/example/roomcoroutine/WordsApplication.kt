93bde41eae7e13916ea54369ec3d4ecdeaa8b5a6
firza122 <firzaajikarya112@gmail.com>
2023-10-13 23:07:32 +0700

    membuat instance repositori dan database

---
+++ app/src/main/java/com/example/roomcoroutine/WordsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.roomcoroutine
+
+import android.app.Application
+
+class WordsApplication : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+
+}
